3b23a22de3aa8660d90239f910682213088524d6
Vlad Varfolomeev <vlad708.var@gmail.com>
2018-04-18 22:50:42 +0600

    Restrusture of page with products

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -8,13 +8,17 @@ class Product extends Component {
 		const { productItems } = this.props;
 
 		return productItems.map(({name, os, battery, camera, size}) => {
-			let imgUrl = toCamelCase(name) + '.png';
-			console.log(imgUrl, '===image-url');
+			let imgUrl = process.env.PUBLIC_URL + '/images/phones/' + toCamelCase(name) + '.png';
+
 			return (
       	<div className="product-item pure-u-1-4">
-					<img src={import(`${imgUrl}`)} />
-					<h4 href="#" className="product-item__title">{name}</h4>
+					<img src={imgUrl} className="product-item__image" />
+					<a href="#" className="product-item__title">{name}</a>
 					<p className="product-item__details">[{os} {battery} {camera} {size}]</p>
+					<a class="pure-button product-item__buy" href="#">
+				    <i class="fa fa-shopping-cart"></i>
+						Купить
+					</a>
 				</div>
 			)
 		});
